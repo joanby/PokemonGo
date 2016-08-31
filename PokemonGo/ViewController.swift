ca5ad107c5f5fcdbae2fd56c6a3a35ecc540d2bf
Juan Gabriel <juangabriel@frogames.es>
2016-08-31 10:57:36 +0200

    Commit inicial de Pokemon Go

---
+++ PokemonGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonGo
+//
+//  Created by Juan Gabriel Gomila Salas on 31/8/16.
+//  Copyright © 2016 Juan Gabriel Gomila Salas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
