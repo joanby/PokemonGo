0a2c6e19ac131df007cd60d1ee6076ee6d1f2a09
Juan Gabriel <juangabriel@frogames.es>
2016-09-01 09:18:37 +0200

    - Implementada la lógica del mapa - Añadidos los gráficos del videojuego

--- PokemonGo/ViewController.swift
+++ PokemonGo/ViewController.swift
@@ -7,11 +7,31 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    
+    var updateCount = 0
+    
+    let mapDistance : CLLocationDistance = 300
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.manager.delegate = self
+        
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("Estamos listos para salir a cazar Pokemons")
+            self.mapView.showsUserLocation = true
+            self.manager.startUpdatingLocation()
+        } else {
+            self.manager.requestWhenInUseAuthorization()
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +40,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    //MARK: Core Location Manager Delegate
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if updateCount < 4 {
+            if let coordinate = self.manager.location?.coordinate {
+                let region = MKCoordinateRegionMakeWithDistance(coordinate, mapDistance, mapDistance)
+                self.mapView.setRegion(region, animated: true)
+                updateCount += 1
+            }
+        } else {
+            self.manager.stopUpdatingLocation()
+        }
+        
+    }
+    
+    @IBAction func updateUserLocation(_ sender: UIButton) {
+        if let coordinate = self.manager.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(coordinate, mapDistance, mapDistance)
+            self.mapView.setRegion(region, animated: true)
+        }
+    }
 
 }
 
